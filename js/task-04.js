d2b8a56923bb68d9acf357ef4b5061157f5f1399
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-12 00:02:40 +0200

    Update task-04.js

--- js/task-04.js
+++ js/task-04.js
@@ -1,4 +1,18 @@
 let counterValue = 0;
 
-const actions = document.querySelectorAll('#counter button');
-console.log(actions);
+const value = document.querySelector('#value');
+const decrementBtn = document.querySelector('#counter button[data-action="decrement"]');
+const incrementBtn = document.querySelector('#counter button[data-action="increment"]');
+
+incrementBtn.addEventListener('click', incrementBtnClickHandler);
+decrementBtn.addEventListener('click', decrementBtnClickHandler);
+
+function incrementBtnClickHandler() {
+  counterValue += 1;
+  value.innerHTML = counterValue;
+}
+
+function decrementBtnClickHandler() {
+  counterValue -= 1;
+  value.innerHTML = counterValue;
+}
