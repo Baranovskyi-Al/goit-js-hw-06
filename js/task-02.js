ea72e915cd1cef9dbb5ce92f11223dd9389fc565
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-11 11:34:49 +0200

    Update task-02.js

--- js/task-02.js
+++ js/task-02.js
@@ -1,8 +1,13 @@
-const ingredients = [
-  'Potatoes',
-  'Mushrooms',
-  'Garlic',
-  'Tomatos',
-  'Herbs',
-  'Condiments',
-];
+const ingredients = ['Potatoes', 'Mushrooms', 'Garlic', 'Tomatos', 'Herbs', 'Condiments'];
+
+const ingredientsList = document.querySelector('#ingredients');
+
+const elements = ingredients.map(ingredient => {
+  const ingredientsItems = document.createElement('li');
+  ingredientsItems.textContent = ingredient;
+  ingredientsItems.classList.add('item');
+
+  return ingredientsItems;
+});
+
+ingredientsList.append(...elements);
