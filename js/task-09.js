e908ab5428ee17bc9bc04804975c7c41ee9e9d67
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-12 14:04:48 +0200

    Update task-09.js

--- js/task-09.js
+++ js/task-09.js
@@ -1,3 +1,14 @@
 function getRandomHexColor() {
   return `#${Math.floor(Math.random() * 16777215).toString(16)}`;
 }
+
+const widget = document.querySelector('div.widget');
+const widgetButtonColorChange = document.querySelector('div.widget button.change-color');
+const colorValue = document.querySelector('div.widget span.color');
+
+widgetButtonColorChange.addEventListener('click', OnClickWidgetButtonColorChange);
+
+function OnClickWidgetButtonColorChange() {
+  document.body.style.backgroundColor = getRandomHexColor();
+  colorValue.innerHTML = getRandomHexColor();
+}
