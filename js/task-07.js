1ff2a52d31168f7d35b9b30f4e73da24a19cffd1
Oleksandr Baranovskyi <89201309+Baranovskyi-Al@users.noreply.github.com>
2022-01-14 10:49:42 +0200

    fix task7

--- js/task-07.js
+++ js/task-07.js
@@ -1,6 +1,10 @@
 const inputFontSize = document.querySelector('#font-size-control');
 const textFontSize = document.querySelector('#text');
 
+console.log(inputFontSize.value);
+
+textFontSize.style.fontSize = `${inputFontSize.value}px`;
+
 inputFontSize.addEventListener('input', onInputFontSizeChange);
 
 function onInputFontSizeChange(event) {
